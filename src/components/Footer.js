1670879cc47d8c88df284e06a58d702218b00980
Inevera11 <zuzannabozek2002@gmail.com>
2022-10-09 11:26:02 +0200

    deployed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import StartButton from "./StartButton";
 import styled from "styled-components";
 import CopmaringStrings from "./CopmaringStrings";
